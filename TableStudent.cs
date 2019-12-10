892695a916866fd3a74f51978f6b64339cc98b27
Ильяс Хуббатулин <ilyuac1998@gmail.com>
2019-12-10 18:00:38 +0300

    Work with Student

--- TableStudent.cs
+++ TableStudent.cs
@@ -17,11 +17,76 @@ namespace BDApp
             InitializeComponent();
         }
 
-
-
         private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void butFrist_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butLast_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butPrevios_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butDel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMoveFirstItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMovePreviousItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMoveNextItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorMoveLastItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripSaveItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
