aba6264ae7d6d91a92be0139c316dec24d8188c8
Ильяс Хуббатулин <ilyuac1998@gmail.com>
2019-12-13 08:54:37 +0300

    save

--- TableMark.cs
+++ TableMark.cs
@@ -8,6 +8,7 @@ namespace BDApp
     public partial class TableMark : Form
     {
         DataTable table = new DataTable();
+        DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
 
         public TableMark()
         {
@@ -15,6 +16,7 @@ namespace BDApp
 
             LoadForm();
 
+            cBFamily.SelectedIndex = 1;
             cBFamily.SelectedIndex = 0;
         }
 
@@ -29,14 +31,14 @@ namespace BDApp
             table.Columns[1].ColumnName = "Предмет";
             table.Columns[2].ColumnName = "Оценка";
 
-            DataTable table1 = DataBase.SelectCommand("SELECT Family FROM Students");
+            DataTable table1 = DataBase.SelectCommand("SELECT * FROM Students");
 
             cBFamily.DataSource = table1;
             cBFamily.DisplayMember = "Family";
+            cBFamily.ValueMember = "IDStudent";
 
             dGV.Columns.Add("Фамилия", "Фамилия");
             dGV.Columns[0].Visible = false;
-            DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
             comboBoxColumn.Name = "Предмет";
             comboBoxColumn.DataSource = DataBase.SelectCommand("SELECT * FROM Subjects");
             comboBoxColumn.DisplayMember = "SubjectName";
@@ -63,20 +65,45 @@ namespace BDApp
 
         private void UpdateData(int index)
         {
-            string command = "UPDATE Tables SET Mark=@mark, IDSubject=" +
-                "(SELECT IDSubject FROM Subjects WHERE SubjectName=@name), " +
-                "IDStudent=(SELECT IDStudent FROM Students WHERE Family=@stname)";
-            OleDbCommand commandUp = new OleDbCommand(command);
+            if (dGV.Rows[index].Cells["Фамилия"].Value != null)
+            {
+                string commandStr = "UPDATE Tables SET Mark='@mark', IDSubject='@sub', IDStudent='@stud'";
+                OleDbCommand command = new OleDbCommand(commandStr,DataBase.connection);
 
-            commandUp.Parameters.AddWithValue("mark", dGV.Rows[index].Cells["Оценка"].Value);
-            commandUp.Parameters.AddWithValue("name", dGV.Rows[index].Cells["Предмет"].Value);
-            commandUp.Parameters.AddWithValue("stname", dGV.Rows[index].Cells["Фамилия"].Value);
+                command.Parameters.AddWithValue("mark", dGV.Rows[index].Cells["Оценка"].Value.ToString());
+                int n = ((DataGridViewComboBoxCell)dGV.Rows[index].Cells["Предмет"]).Items.IndexOf(
+                            ((DataGridViewComboBoxCell)dGV.Rows[index].Cells["Предмет"]).Value.ToString())+2;
+                command.Parameters.AddWithValue("sub", n);
+                command.Parameters.AddWithValue("stud", cBFamily.SelectedItem.ToString()) ;
+
+                DataBase.DBCommand(command);
+            }
+            //else
+            //{
+            //    if (dGV.Rows[index].Cells["Оценка"].Value != null && dGV.Rows[index].Cells["Предмет"].Value!= null)
+            //    {
+            //        string commandStr = "INSERT INTO Tables ([Mark], [IDSubject], [IDStudent])" +
+            //        "VALUES (@mark,(SELECT IDSubject FROM Subjects WHERE SubjectName=@name), " +
+            //        "(SELECT IDStudent FROM Students WHERE Family=@stname))";
+            //        OleDbCommand command = new OleDbCommand(commandStr, DataBase.connection);
+
+            //        command.Parameters.AddWithValue("mark", dGV.Rows[index].Cells["Оценка"].Value.ToString());
+            //        command.Parameters.AddWithValue("name", dGV.Rows[index].Cells["Предмет"].Value.ToString());
+            //        command.Parameters.AddWithValue("stname", cBFamily.Text);
+
+
+            //        DataBase.DBCommand(command);
+            //    }
+            //}
+
+
         }
 
         private void butSave_Click(object sender, EventArgs e)
         {
             for (int i = 0; i < dGV.Rows.Count; i++)
-                UpdateData(i);
+                if(dGV.Rows[i].Cells["Оценка"].Value !=null)
+                    UpdateData(i);
         }
     }
 }
